366b986e58d8cae9ed5b04bcca48e5067d457862
Debarghya Saha <72520073+debarghyaRONIN@users.noreply.github.com>
2025-06-22 02:43:42 +0530

    Create gtag.js

---
+++ app/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZDVTBDP9T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FZDVTBDP9T');
+</script>
